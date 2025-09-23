ca7ef5679a1d209ea5f0d53b13989a60f36c8cd9
italoo97 <italohugodf39@gmail.com>
2025-09-23 18:10:19 -0300

    Template atualizado e adicionado search na views

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-	cars = Car.objects.filter() #(brand__name='Fiat') #aqui se usa 2 _ para indicar que voce quer pesquisar pelo nome e não pelo ID do item 
+	cars = Car.objects.all()
+	search = request.GET.get('search')
+	if search:
+		cars = Car.objects.filter(model__contains=search) #(brand__name='Fiat') #aqui se usa 2 _ para indicar que voce quer pesquisar pelo nome e não pelo ID do item 
 	
 	return render(
 				request, 
