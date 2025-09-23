8caf6770947959e75fe393dbd35ea1aba7a94b87
italoo97 <italohugodf39@gmail.com>
2025-09-23 14:53:00 -0300

    Adicionado templates

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+def cars_view(request):
+	cars = Car.objects.filter() #(brand__name='Fiat') #aqui se usa 2 _ para indicar que voce quer pesquisar pelo nome e não pelo ID do item 
+	
+	return render(
+				request, 
+				'cars.html',
+				{'cars': cars} #Aqui é um dicionario para simbolizar o banco de dados
+				)
